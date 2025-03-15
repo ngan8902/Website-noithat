4e44f203b15a8c375cdd7007bd1c1decd833d2ee
ngan8902 <bichnganb722@gmail.com>
2025-03-15 12:22:36 +0700

    notify of cart

---
+++ frontend/src/constants/notify.constant.js
@@ -0,0 +1,14 @@
+import { toast } from "react-toastify";
+
+export const notifyOfCart = () => {
+    toast.success("🛒 Đã thêm vào giỏ hàng!", {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: false,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+      });
+}
