272a115e6d6a0b901b97187d6991c49746a37f60
ngan8902 <bichnganb722@gmail.com>
2025-04-29 11:53:00 +0700

    fix

--- frontend/src/pages/Employee.js
+++ frontend/src/pages/Employee.js
@@ -9,7 +9,7 @@ const Employee = () => {
     <div className="d-flex app-container">
       <Sidebar />
       <div className="content p-4 main-content">
-        <h2 className="text-center fw-bold mb-4">Quản Lý Nhân Sự</h2>
+        <h2 className="text-center fw-bold mb-4">Quản Lý Nhân Viên</h2>
         <EmployeeList employees={employees} setEmployees={setEmployees} />
       </div>
     </div>
